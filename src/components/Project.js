2cab6d221d3fceeddaa32490ba670678d2b29565
Junghun Kim <junghun1112@gmail.com>
2023-03-30 06:31:26 -0500

    firebase done

---
+++ src/components/Project.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Project() {
+  return <h1>To Be Updated By April</h1>;
+}
---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfI7xnvv6LvJB6PmBiqaaFom50ORq73A8",
+  authDomain: "jeonghoonkim-d4f87.firebaseapp.com",
+  projectId: "jeonghoonkim-d4f87",
+  storageBucket: "jeonghoonkim-d4f87.appspot.com",
+  messagingSenderId: "189143961814",
+  appId: "1:189143961814:web:a4e8dd6cf33d9b4775f977",
+  measurementId: "G-Z5M3YFF3QS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
