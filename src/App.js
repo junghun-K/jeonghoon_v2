0c8e11ea0405c3fc5b1662850a34a4275560a004
Junghun Kim <junghun1112@gmail.com>
2023-03-30 23:39:04 -0500

    project added

--- src/App.js
+++ src/App.js
@@ -18,7 +18,20 @@ import TimelineDot from '@mui/lab/TimelineDot';
 import DescriptionIcon from '@mui/icons-material/Description';
 import Resume from './components/resume.pdf';
 import Rating from '@mui/material/Rating';
-// skills imgs
+
+// Projects
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
+
+
+
+// Skills
 import Python from './images/python.png';
 import Pytorch from './images/pytorch.png';
 import Git from './images/git.png';
@@ -43,10 +56,26 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
 const skills = ['Python', 'Pytorch', 'Tensorflow', 'Javascript', 'React', 'Node.js', 'HTML', 'CSS', 'Postman', 'SQL', 'Java', 'Docker', 'Git'];     
 
 function App() {
-  
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <Container maxWidth={false} disableGutters>
       <AppBar position="static"
@@ -164,7 +193,7 @@ function App() {
         </Container>
         
       </Container>    
-      {/* Experience / Education & Skills*/}
+      {/* Experience / Education & & Skills*/}
       <Container maxWidth={false}
       sx={{ 
         marginTop: '20px',
@@ -278,7 +307,72 @@ function App() {
           <Button variant="outlined" href={Resume} target="_blank" startIcon={<DescriptionIcon />}>
             resume
           </Button>
+          
+        {/* PROJECTS */}
+        <Container sx={{marginTop: '80px', mx: '0px'}}>
+          <Typography variant='h4'
+            sx={{fontSize: '1rem', fontWeight:'700', color: '#134db6', letterSpacing: '3px', marginBottom: '20px'}}>
+            PROJECTS
+          </Typography>
+          <Grid container spacing={40} justifyContent="center" sx={{display:'flex'}}>
+            <Grid item>
+            <Card sx={{ maxWidth: 300 }}>
+              <CardHeader
+                title="Shrimp and Chorizo Paella"
+                subheader="September 14, 2016"
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  This impressive paella is a perfect party dish and a fun meal to cook
+                  together with your guests. Add 1 cup of frozen peas along with the mussels,
+                  if you like.
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing>
+                <ExpandMore
+                  expand={expanded}
+                  onClick={handleExpandClick}
+                  aria-expanded={expanded}
+                  aria-label="show more"
+                >
+                  <ExpandMoreIcon />
+                </ExpandMore>
+              </CardActions>
+              <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                  <Typography paragraph>Method:</Typography>
+                  <Typography paragraph>
+                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+                    aside for 10 minutes.
+                  </Typography>
+                  <Typography paragraph>
+                    Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+                    medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+                    occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+                    large plate and set aside, leaving chicken and chorizo in the pan. Add
+                    pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+                    stirring often until thickened and fragrant, about 10 minutes. Add
+                    saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                  </Typography>
+                  <Typography paragraph>
+                    Add rice and stir very gently to distribute. Top with artichokes and
+                    peppers, and cook without stirring, until most of the liquid is absorbed,
+                    15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+                    mussels, tucking them down into the rice, and cook again without
+                    stirring, until mussels have opened and rice is just tender, 5 to 7
+                    minutes more. (Discard any mussels that don&apos;t open.)
+                  </Typography>
+                  <Typography>
+                    Set aside off of the heat to let rest for 10 minutes, and then serve.
+                  </Typography>
+                </CardContent>
+              </Collapse>
+            </Card>
+            </Grid>
+          </Grid>
+        </Container>
 
+        {/* SKILLS */}
         <Container sx={{marginTop: '80px', mx: '0px'}}>
           <Typography variant='h4'
           sx={{fontSize: '1rem', fontWeight:'700', color: '#134db6', marginBottom: '30px', letterSpacing: '3px'}}>
